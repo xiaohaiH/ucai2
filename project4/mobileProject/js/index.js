3aeafbf4b7f5e8af120216908bf8f6254e2a8ab4
肖海东? <773884562@qq.com>
2017-06-21 23:46:00 +0800

    abc

--- project4/mobileProject/js/index.js
+++ project4/mobileProject/js/index.js
@@ -10,21 +10,45 @@
         "success": function(data) {
             // console.log(data);
             // console.log(data.subjects[0].title) ///标题.
-            // console.log(data.subjects[0].images.large) ///图片地址. 
-            // console.log(data.subjects[0].rating.average) ///评分. 
+            // console.log(data.subjects[0].images.large) ///图片地址.
+            // console.log(data.subjects[0].rating.average) ///评分.
             var tagText = $("#templete").html();
-            var text = "";
-            console.log(tagText);
-
+            var $box = $(".banner .row");
+            var $leftBtn = $(".banner .bannerLeft");
+            var $rightBtn = $(".banner .bannerRight");
+            var num = 0;
+            var init = 0;
             $.each(data.subjects, function(index, val) {
-                // text +=
-                // $(".banner .row").html()
-                console.log(val);
-                console.log(val.title);
-                // function tagValue(value) {
-                //     var reg = /^<%=   %>/g;
-                // };
-            })
+                num = index;
+                var that = this;
+                function getTitle(){
+                    that.ttle = {};
+                    that.ttle.title = that.title;
+                    var reg = /\<\%\=\s+(\w+)[\.|](\w+)\s+\%\>/g;
+                    return tagText.replace(reg,function(index,$1,$2){
+                        return that[$1][$2];
+                    })
+                };
+                $box.append(getTitle());
+            });
+            $box.css({"width":++num * 10 + "rem"});
+            $leftBtn.on("click",toLeft);///监听点击左侧的按钮.
+            $rightBtn.on("click",toRight);///监听点击右侧的按钮.
+            function toLeft(){///监听点击左侧的按钮.
+                init -= 20;
+                if(init <= 0){
+                    init = 0;
+                }
+                $box.css({"left":-init + "rem"});
+            };
+            function toRight(){///监听点击右侧的按钮.
+                init += 20;
+                var maxNum = parseInt($box.css("width")) / 20 - 36;
+                if(init >= maxNum){
+                    init = maxNum;
+                };
+                $box.css({"left":-init + "rem"});
+            };
         }
     })
 }())
