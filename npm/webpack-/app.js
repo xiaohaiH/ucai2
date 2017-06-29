8a79261fa8291e1f468956b83cf8d80d6941b56e
xiaohaiH <773884562@qq.com>
2017-06-29 21:44:10 +0800

    webpack

---
+++ npm/webpack-/app.js
@@ -0,0 +1,5 @@
+
+import index from "./src/index.html";
+
+import js from "./src/js/index.js";
+import css from "./src/css/index.css";
---
+++ npm/webpack-/webpack.config.js
@@ -0,0 +1,51 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");///webpack2要求必须写.
+// module.exports = {
+//   entry: "./app.js",
+//   output: {
+//     path: __dirname + '/dist',
+//     filename: 'index_bundle.js'
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: 'My App',
+//       filename: 'assets/admin.html',
+//       template : "./src/index.html",
+//     })
+//   ]
+// }
+	module.exports = {
+		entry : ["./app.js"],///入口文件.
+		output : {///输出路径.
+			path : path.resolve(__dirname,"./dist/js/"),
+			filename: "bundle.js"
+		},
+		module : {
+			loaders :[
+				{
+					test : /\.css$/,
+                	include: path.resolve(__dirname, 'src'),///设置检索的相对路径.
+                	exclude: path.resolve(__dirname, 'node_modules'),
+					use: [
+	                    { loader: 'style-loader' },
+	                    { loader: 'css-loader' },
+	                    // { loader: 'postcss-loader' }
+	                ]
+				},
+				{
+					test : /\.js$/,
+					// use : {loader : "bable-loader"}
+				},
+				{
+					test : /\.html$/,///这个是在js中直接引入HTML时,需要这样来转义成js里的字符串.
+					use :
+						{loader : 'html-loader'}
+				}
+			]
+		},
+		plugins : [new HtmlWebpackPlugin({///这个是以自定义的HTML文件为模板来生成HTML文件,需要下载包并在顶部引入.
+			template : "./src/index.html",
+			filename : "[hash]-test.html",
+			title : "welcome to hai's home",///当用了模板时,title不会生效,而不用模板时title则在默认index.html里生效.
+		})]
+	};
