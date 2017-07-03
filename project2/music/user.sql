3ef7fc948c11bde7ed0a79bc9c1d79e650e1603c
xiaohaiH <773884562@qq.com>
2017-07-03 19:50:26 +0800

    项目二

---
+++ project2/music/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `name`, `password`) VALUES (1, 'xxxxxxx', 'hhhhhhh');
+INSERT INTO `user` (`id`, `name`, `password`) VALUES (2, 'qqqqqqqqq', 'ae0469e4ce1f53a2e95399bf2333d7eb');
+INSERT INTO `user` (`id`, `name`, `password`) VALUES (3, 'iiiiiiiiiiiiiiiiiiiiiiii', 'c5b696acee03e35b568868db1c6f2627');
